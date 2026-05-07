8360e968d8b93a08186406be63c6a6e5cb1aa9cf
Danielle Thompson <shortydee34@gmail.com>
2026-05-07 10:23:44 -0700

    Add initial script.js with video and menu functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+/*
+Student Name: Danielle Thompson
+File Name; script.js
+Date: 2-20-2025
+*/
+
+//Global variables 
+var video = document.getElementById ("example");
+var videoSource = document.getElementById ("vid-src");
+var descriptionSource = document.getElementById ("despsrc");
+
+
+function hamburger() {
+    let menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
 
